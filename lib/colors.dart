a190cc145856f2fa960c5cf8a72bedbc401b9b03
Gisiora Elvis <gisioraelvis20@gmail.com>
2022-05-11 15:50:43 +0300

    color constants

---
+++ lib/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color rwColor = Color.fromRGBO(4, 43, 89, 1);
+const TextStyle focusedStyle = TextStyle(color: Colors.blue);
+const TextStyle unfocusedStyle = TextStyle(color: Colors.grey);

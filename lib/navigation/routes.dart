53d82b14eacb301289293b8f48eef47114e26342
Gisiora Elvis <gisioraelvis20@gmail.com>
2022-05-11 16:06:02 +0300

    navigation, the routes and router

---
+++ lib/navigation/routes.dart
@@ -0,0 +1,8 @@
+class AppScreens {
+  static String splash = 'splash';
+  static String onboarding = 'onboarding';
+  static String signin = 'signin';
+  static String signup = 'signup';
+  static String home = 'home';
+  static String profile = 'profile';
+}
